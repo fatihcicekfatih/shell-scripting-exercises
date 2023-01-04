76b7a386cb116c70be04044dfa661b9d3077fed4
Fatih <68778759+fatihcicekfatih@users.noreply.github.com>
2023-01-04 10:55:33 +0300

    for-loop file is added.

---
+++ for-loop.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# echo "Numbers:"
+
+# for number in 0 1 2 3 4 5 6 7 8 9
+# do
+#    echo $number
+# done
+
+# for number in {0..10}
+# do
+#     echo $number
+#     sleep 1
+# # done
+
+# echo "Names:"
+
+# for name in Fabian Lion Muhammet
+# do
+#    echo $name
+# done
+
+# echo "Files in current folder:"
+
+# for file in `pwd`/*
+# do
+#    echo $file
+# done
+
+# number=1
+# until [[ $number lt 1 ]]
+# do
+#     echo $number
+#     ((number++))
+# done
+# echo "Now, number is $number"
+
+#!/bin/bash
+
+# for name in Fatih Aydin Sule Adnan Emre
+# do
+#     sudo userdel $name
+# done
+
+# tail -5 /etc/passwd
+
+
